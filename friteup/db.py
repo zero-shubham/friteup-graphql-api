79ef9e700bffe8d201cfc76cfad33e4fdeed4bd9
Shubham Biswas <shubhambiswas.zero@gmail.com>
2020-02-04 23:11:52 +0530

    adding loop to motor

--- friteup/db.py
+++ friteup/db.py
@@ -1,5 +1,9 @@
 import motor.motor_asyncio
+import asyncio
 
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
 # 'mongodb+srv://zero:zero000!@friteup-o8pmp.mongodb.net/test?retryWrites=true&w=majority'
-client = motor.motor_asyncio.AsyncIOMotorClient('mongodb+srv://zero:zero0000@friteup-o8pmp.mongodb.net/test?retryWrites=true&w=majority')
+client = motor.motor_asyncio.AsyncIOMotorClient(
+    'mongodb+srv://zero:zero0000@friteup-o8pmp.mongodb.net/test?retryWrites=true&w=majority', io_loop=loop)
 db = client.test_database
