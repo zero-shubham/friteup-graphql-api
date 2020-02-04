e158c34046ef0ae40f3b30adfebcea40729201dd
Shubham Biswas <shubhambiswas.zero@gmail.com>
2020-02-04 22:45:50 +0530

    test

--- friteup/application.py
+++ friteup/application.py
@@ -85,4 +85,4 @@ async def cookie_set(request: Request, call_next):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    uvicorn.run("application:app", host="127.0.0.1", port=port, log_level="info")
+    uvicorn.run("application:app", host="0.0.0.0", port=port, log_level="info")
