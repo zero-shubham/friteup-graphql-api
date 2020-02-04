e6adfd9f2bda60fb6fae20f640d7d759c0bcd4e6
Shubham Biswas <shubhambiswas_zero@yahoo.com>
2020-02-05 00:10:32 +0530

    Update db.py

--- friteup/db.py
+++ friteup/db.py
@@ -1,5 +1,5 @@
 import motor.motor_asyncio
 
-# 'mongodb+srv://zero:zero000!@friteup-o8pmp.mongodb.net/test?retryWrites=true&w=majority'
+
 client = motor.motor_asyncio.AsyncIOMotorClient()
 db = client.test_database
